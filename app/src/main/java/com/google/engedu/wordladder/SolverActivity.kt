9c1b99fa508b10326d16e927a692c53e14521c63
shizhanxu <copernicus.xu@gmail.com>
2020-09-22 16:57:05 +0800

    Create lines of EditText according to the size of word path

--- app/src/main/java/com/google/engedu/wordladder/SolverActivity.kt
+++ app/src/main/java/com/google/engedu/wordladder/SolverActivity.kt
@@ -3,6 +3,9 @@ package com.google.engedu.wordladder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.engedu.worldladder.R
 
@@ -11,10 +14,18 @@ class SolverActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_solver)
         val path = intent.getStringArrayExtra("Path")!!
-//        for (s in path) {
-//            Log.d("sb", "onCreate: $s")
-//        }
-        findViewById<TextView>(R.id.startTextView).text = path.first()
-        findViewById<TextView>(R.id.endTextView).text = path.last()
+        for (s in path) {
+            Log.d("sb", "onCreate: $s")
+        }
+        val startTextView = findViewById<TextView>(R.id.startTextView).apply {
+            text = path.first()
+        }
+        val endTextView = findViewById<TextView>(R.id.endTextView).apply {
+            text = path.last()
+        }
+        val wordLadder = findViewById<LinearLayout>(R.id.wordLadder)
+        for (i in 1..path.size - 2) {
+            wordLadder.addView(EditText(this), 1)
+        }
     }
 }
